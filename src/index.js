b3c6e1f295072271ca44ef4e97a11d6a561ad967
ParkerCatz <tksm.webdesign09@gmail.com>
2021-02-18 23:23:43 +0900

    Revert "test: husky&lint-stagedの確認"
    This reverts commit 051af5ab09b1ed8d153b6ef08adcae7431b5f39e.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import { hello } from "./js/sub"
-import "./scss/style.scss"
+import { hello } from './js/sub'
+import './scss/style.scss'
 
-console.log("Hello World")
+console.log('Hello World')
 
 hello()
 
-window.addEventListener("load", () => {
-  console.log("IE11で表示されたらJavaScript Transpile成功")
+window.addEventListener('load', () => {
+  console.log('IE11で表示されたらJavaScript Transpile成功')
 })
-console
